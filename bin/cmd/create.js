a614d0c5deac6bad7ebb9cba8133a656105906dd
oscar.wl <oscar.wl@alibaba-inc.com>
2020-09-21 17:16:30 +0800

    fix: 调整参数

--- bin/cmd/create.js
+++ bin/cmd/create.js
@@ -1,64 +1,60 @@
-const fs = require('fs');
-const rimraf = require('rimraf');
-const child_process = require('child_process');
+const fs = require("fs");
+const rimraf = require("rimraf");
+const child_process = require("child_process");
 
-const checkArg = require('../utils/checkArg.js');
+const checkArg = require("../utils/checkArg.js");
 
-module.exports = (projectName, language) => {
+module.exports = (projectName) => {
   checkArg(
     projectName.match(/^[a-z][a-z0-9_-]+$/),
     `The project must start with a letter and can only contain these symbols: a-z, 0-9, -, _`
   );
 
   checkArg(
-    ['js', 'ts'].indexOf(language) !== -1,
-    `The project language should be in: ts, js`
-  );
-
-  checkArg(
-   !fs.existsSync(`./${projectName}`),
+    !fs.existsSync(`./${projectName}`),
     `File or folder "./${projectName}" already exists`
   );
 
-  const repo = `git@gitlab.alibaba-inc.com:deepinsight/di-open-demo-${language}.git`;
+  const repo = `git@gitlab.alibaba-inc.com:qbi/bi-component.git`;
 
   console.log(`Creating new project "${projectName}" now!`);
-  console.log('');
+  console.log("");
 
   const execSync = child_process.execSync;
-  const cmd = `git clone --quiet --depth=1 --branch=master ${repo} ./${projectName}`;
+  const cmd = `git clone --quiet --depth=1 --branch=react-rewired ${repo} ./${projectName}`;
 
   console.log(`--> Running: ${cmd}`);
   execSync(cmd);
 
   const projectPackage = `./${projectName}/package.json`;
 
-  fs.readFile(projectPackage, 'utf8', function(err, data) {
+  fs.readFile(projectPackage, "utf8", function (err, data) {
     if (err) {
       rimraf.sync(`./${projectName}`);
-      console.log('--> Clone failed');
+      console.log("--> Clone failed");
       return console.log(err);
     }
-    console.log('--> Clone succeeded');
+    console.log("--> Clone succeeded");
 
     console.log(`--> Removing ./${projectName}/.git`);
     rimraf.sync(`./${projectName}/.git`);
 
-    console.log('--> Updating package.json');
+    console.log("--> Updating package.json");
 
     const json = JSON.parse(data);
     json.name = projectName;
-    json.author = 'Write your name here';
-    json.version = '0.1.0';
-    // 修改打包的文件名
-    json.scripts.zip = json.scripts.zip.replace('di-open-demo', projectName);
+    json.author = "Write your name here";
+    json.version = "0.1.0";
 
-    fs.writeFile(projectPackage, JSON.stringify(json, null, 2), function() {
-      console.log('');
-      console.log(`执行完毕！下一步:`);
+    // 替换打包文件名为项目名称
+    json.scripts.zip = json.scripts.zip.replace("component", projectName);
+
+    fs.writeFile(projectPackage, JSON.stringify(json, null, 2), function () {
+      console.log("");
+      console.log(`qbi项目初始化完成！下一步:`);
       console.log(`  cd ${projectName}`);
-      console.log(`  tnpm i`);
-      console.log(`  tnpm start`);
+      console.log(`  npm i`);
+      console.log(`  npm start`);
     });
   });
 };
--- bin/index.js
+++ bin/index.js
@@ -1,19 +1,16 @@
 #! /usr/bin/env node
-const program = require('commander');
+const program = require("commander");
 
-const packageJson = require('../package.json');
+const packageJson = require("../package.json");
 
-program
-  .version(packageJson.version, '-v, --version')
-  .usage('<di-cli>');
+program.version(packageJson.version, "-v, --version").usage("<di-cli>");
 
 program
-  .command('create <project_name>')
-  .description('create a demo component project')
-  .option('-l, --language [js / ts]', 'Set the project language, default is js')
-  .usage('<project_name> [options]')
+  .command("create <project_name>")
+  .description("create a demo component project")
+  .usage("<project_name> [options]")
   .action((project_name, cmd) => {
-    require('./cmd/create')(project_name, cmd.language || 'js');
+    require("./cmd/create")(project_name);
   });
 
 program.parse(process.argv);
