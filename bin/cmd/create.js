986e4cb8e3aa65aa487fe78f5b4bb190002c5c2c
oscar.wl <oscar.wl@alibaba-inc.com>
2020-10-19 11:42:14 +0800

    fix: deploy tools

--- bin/cmd/create.js
+++ bin/cmd/create.js
@@ -30,10 +30,10 @@ module.exports = (projectName) => {
   fs.readFile(projectPackage, "utf8", function (err, data) {
     if (err) {
       rimraf.sync(`./${projectName}`);
-      console.log(chalk.red("--> create failed"));
+      console.log(chalk.red("  create failed"));
       return console.log(err);
     }
-    console.log(chalk.green("--> create succeeded"));
+    console.log(chalk.green("  create succeeded"));
 
     rimraf.sync(`./${projectName}/.git`);
 
@@ -47,7 +47,7 @@ module.exports = (projectName) => {
 
     fs.writeFile(projectPackage, JSON.stringify(json, null, 2), function () {
       console.log("");
-      console.log(chalk.green(`Project initialize succeed! Next step:`));
+      console.log(chalk.green(`Project initialized successfully! Next step:`));
       console.log(chalk.blue(`  cd ${projectName}`));
       console.log(chalk.blue(`  npm i`));
       console.log(chalk.blue(`  npm start`));
