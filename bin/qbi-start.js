157b1319c5dd4b5b2d60d5a856290ff288f19217
haili.lhq <haili.lhq@alibaba-inc.com>
2021-03-30 16:14:46 +0800

    fix: checkout host and port before starting dev server

--- bin/qbi-start.js
+++ bin/qbi-start.js
@@ -3,6 +3,7 @@ const WebpackDevServer = require('webpack-dev-server');
 const logger = require('./utils/logger');
 const { getWebpackConfig } = require('./utils/webpack-config');
 const webpack = require('webpack');
+const net = require('net');
 
 const options = process.argv;
 program.option('-a, --analyze [analyze]', 'Analyze the bundle', false);
@@ -10,29 +11,31 @@ program.option('-a, --analyze [analyze]', 'Analyze the bundle', false);
 program.parse(options);
 
 let server;
-try {
-  logger.blank();
-  logger.info('Starting server...');
-  const webpackConfig = getWebpackConfig({
-    mode: 'development',
-    analyze: program.analyze,
-  });
-  let compiler = webpack(webpackConfig);
+logger.blank();
+logger.info('Starting server...');
+const webpackConfig = getWebpackConfig({
+  mode: 'development',
+  analyze: program.analyze,
+});
+checkServer(webpackConfig.devServer.port, webpackConfig.devServer.host)
+  .then(() => {
+    let compiler = webpack(webpackConfig);
 
-  server = new WebpackDevServer(compiler, webpackConfig.devServer);
-  server.listen(
-    webpackConfig.devServer.port,
-    webpackConfig.devServer.host,
-    () => {
-      logger.info(
-        `DevServer listening on port ${webpackConfig.devServer.port}`,
-      );
-    },
-  );
-} catch (err) {
-  logger.error('LOAD ERROR', err);
-  process.exit(1);
-}
+    server = new WebpackDevServer(compiler, webpackConfig.devServer);
+    server.listen(
+      webpackConfig.devServer.port,
+      webpackConfig.devServer.host,
+      () => {
+        logger.info(
+          `DevServer listening on port ${webpackConfig.devServer.port}`,
+        );
+      },
+    );
+  })
+  .catch(err => {
+    logger.error('LOAD ERROR', err);
+    stopServer();
+  });
 
 //clean up
 function stopServer() {
@@ -41,8 +44,24 @@ function stopServer() {
   if (server) {
     server.close();
     server = null;
-    process.exit(1);
   }
+  process.exit(1);
+}
+
+function checkServer(port, host) {
+  return new Promise((resolve, reject) => {
+    const server = net.createServer();
+    server.once('error', err => {
+      reject(err);
+    });
+    server.once('listening', function () {
+      server.close();
+    });
+    server.once('close', function () {
+      resolve();
+    });
+    server.listen(port, host);
+  });
 }
 
 process.on('SIGINT', stopServer);
